b80a2b941aae7ca521d3831d0f477e21e59c6627
morgan berry <morganberry@morgans-MacBook-Pro.local>
2022-04-18 20:23:50 -0600

    Add Js and Index

---
+++ challenge/index.md
@@ -0,0 +1,9 @@
+# 5 Book Titles
+
+1. The Cat in the Hat
+2. The Four Agreements
+3. Variable Types
+4. How to do Math
+5. Another book
+
+Connor and I worked together but I would say he drove and navigated which was helpful for me as I learn through watching.
---
+++ challenge/javascript.js
@@ -0,0 +1,13 @@
+//Its a Javascript file
+
+
+var hairColor = "red"
+var house = "Erie"
+var number = 1
+var numberTwo = -2
+var iHaveRedHair = true
+var dontHaveRedHair = false
+
+var books = ['Cat in the Hat', 'Green Eggs and Ham', 'One fish, Two Fish'];
+
+console.log("I have" + hairColor + "I live in" + house + "I have" + number "cat")
